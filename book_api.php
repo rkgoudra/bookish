a1ef98098b869d92b01b29797aa97b733142167e
rkgoudra <yash.cool995@gmail.com>
2019-10-15 21:56:26 +0530

    book_api crud done

--- book_api.php
+++ book_api.php
@@ -12,23 +12,23 @@ switch($flag){
     $book_status=$_POST['status'];
     $book_description=$_POST['description'];
     $user_id=$_POST['uid'];
-         $sql="INSERT INTO `book_table`(`title`, `image`, `author_name`, `language`, `gener`, `status`, `description`,`user_id`) 
-         VALUES ('$book_title','$book_image','$book_auth_name','$book_language','$book_gener','$book_status','$book_description','$user_id') ";
+    
+         $sql="INSERT INTO `book_table`(`title`, `image`, `author_name`, `language`, `gener`, `status`, `description`,`user_id`,`delete_status`) 
+         VALUES ('$book_title','$book_image','$book_auth_name','$book_language','$book_gener','$book_status','$book_description','$user_id',1) ";
                  $res=mysqli_query($con,$sql);
                          if($res){
                          //echo "inserted sucessfully";
                          $results2['status']=1;
                          $results2['message']="inserted successfull";
-                         array_push($results,$results2);
-                         echo json_encode($results);
                          }else{
                          //echo "failed to insert";
                          $results2['status']=-1;
                          $results2['message']="insert failed";
+                         }
                          array_push($results,$results2);
                          echo json_encode($results);
-                         }
     break;
+    
     //select query 
     case 2:$user_id=$_POST['uid'];
            $sql="SELECT * FROM `book_table` WHERE user_id='$user_id'";
@@ -51,6 +51,41 @@ switch($flag){
          $sql="UPDATE `book_table` SET `title`='$book_title',`image`='$book_image',`author_name`='$book_auth_name',`language`='$book_language',
          `gener`='$book_gener',`status`='$book_status',`description`='$book_description',`user_id`='$user_id' WHERE `book_id`='$book_id'"; 
          $res=mysqli_query($con,$sql);
+         if($res)
+    {
+        //echo "inserted sucessfully";
+        $results2['status']=1;
+        $results2['message']="update successfull";
+    }
+    else{
+        //echo "failed to insert";
+        $results2['status']=-1;
+        $results2['message']="update failed";
+        
+    }
+    array_push($results,$results2);
+    echo json_encode($results);
+    break;
+//delete query
+case 4: $book_id=$_POST['bid'];
+     $sql="UPDATE `book_table` SET `delet_status`= 0 WHERE `book_id`='$book_id'"; 
+     $res=mysqli_query($con,$sql);
+     if($res)
+{
+    //echo "inserted sucessfully";
+    $results2['status']=1;
+    $results2['message']="delete successfull";
+}
+else{
+    //echo "failed to insert";
+    $results2['status']=-1;
+    $results2['message']="delete failed";
+    
+}
+array_push($results,$results2);
+echo json_encode($results);
+break;
+
 
 
 
