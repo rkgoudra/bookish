952e2562fb150cdf5293520d18dfb6f344570197
rkgoudra <yash.cool995@gmail.com>
2019-10-15 22:25:18 +0530

    borrow_book_api crud start

---
+++ borrow_book_api.php
@@ -0,0 +1,18 @@
+<?php
+require 'db.php';
+$results=array();
+$flag=$_POST['flag'];
+switch($flag){
+    //insert query to borrow_book table
+    case 1:
+break;
+//select
+    case 2:
+break;
+
+    case 3:
+break;
+    default: echo "default case";
+break;
+}
+?>
