83b5e7df30cde4c8eae1f9c3bdd1b4b83be134b5
rkgoudra <yash.cool995@gmail.com>
2019-11-06 23:09:58 +0530

    json error Solved in frontend

--- api.php
+++ api.php
@@ -34,28 +34,29 @@ switch($flag)
     $password=md5($_POST['pwd']);
     $phone_no=$_POST['phno'];
    // $status=$_POST['status'];
-    $gender=$_POST['gender'];     
+    //$gender=$_POST['gender'];     
      $sql1 = "SELECT `email_id`, `phone_no` FROM `register` WHERE `email_id`='$email_id' or `phone_no`='$phone_no'" ;
      $res=mysqli_query($con,$sql1);
      if (mysqli_num_rows($res) == 0) {
-      $sql="INSERT INTO `register` (`first_name`, `last_name`, `email_id`, `password`, `phone_no`, `status`, 
-        `gender`) VALUES ('$first_name','$last_name','$email_id','$password','$phone_no',1,'$gender')";
+      $sql="INSERT INTO `register` (`first_name`, `last_name`, `email_id`, `password`, `phone_no`, `status`) 
+      VALUES ('$first_name','$last_name','$email_id','$password','$phone_no',1)";
          $res1=mysqli_query($con,$sql);
          if($res1){
             //echo "inserted sucessfully";
             $results2['status']=1;
-            $results2['message']="inserted successfull";
+            $results2['message']='inserted successfull';
             }else{
             //echo "failed to insert";
             $results2['status']=-1;
-            $results2['message']="insert failed";
+            $results2['message']='insert failed';
             }  
      }else{
         $results2['status']=0;
-        $results2['message']="email or mobile Number found";
+        $results2['message']='email or mobile Number found';
         }
-        array_push($results,$results2);
-        echo json_encode($results); 
+        // array_push($results,$results2);
+        
+        echo json_encode($results2); 
     
     break;
     
